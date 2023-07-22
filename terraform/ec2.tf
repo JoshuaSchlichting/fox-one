862c8f2fe6e6ac306f1709e207dcf3c913e46999
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-07-21 21:40:02 -0400

    g4dn.xlarge instance type, add todo for sound

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,16 +1,15 @@
-
+# TODO: get sound card!
 resource "aws_instance" "gaming" {
   ami               = data.aws_ami.windows_2022.id
   availability_zone = "${var.region}a"
-  # instance_type = "g4dn.xlarge"
-  instance_type               = "t3.2xlarge"
+  # Must be a g* instance type to support NVIDIA GPU
+  instance_type = "g4dn.xlarge"
   associate_public_ip_address = true
   ebs_optimized               = true
   key_name = aws_key_pair.gaming.key_name
   root_block_device {
     volume_size = "512"
-    volume_type = "io1"
-    iops        = "100"
+    volume_type = "gp2"
   }
   security_groups = [aws_security_group.gaming.name]
 
